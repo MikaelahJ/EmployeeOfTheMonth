9775a471b4f80c5c995f1fea86e99ff9fbf85788
MikaelahJ <mikjen0129@skola.goteborg.se>
2022-11-24 16:23:25 +0100

    Added item spawning

--- Employee of the month/Assets/Scripts/ItemSpawner.cs
+++ Employee of the month/Assets/Scripts/ItemSpawner.cs
@@ -10,32 +10,49 @@ public class ItemSpawner : MonoBehaviour
     private float timer;
 
     private List<NewItemScriptableObject> allItems;
+    private List<Transform> allSpawnPoints = new List<Transform>();
+
+    [SerializeField] private GameObject weaponModifyer;
 
     private void Start()
     {
         GetAllItems();
-
+        GetSpawnPoints();
+        SpawnItem();
     }
-
-    private void GetAllItems()
-    {
-        allItems = new List<NewItemScriptableObject>(Resources.LoadAll<NewItemScriptableObject>("ItemData/Items/"));
-        Debug.Log("allitems: " + allItems.Count);
-    }
-
     private void Update()
     {
         timer += Time.deltaTime;
 
         if (timer >= spawnInterval)
         {
-            SpawnItem();
+            //SpawnItem();
         }
+    }
+    private void GetAllItems()
+    {
+        allItems = new List<NewItemScriptableObject>(Resources.LoadAll<NewItemScriptableObject>("ItemData/Items/"));
+        Debug.Log("allitems: " + allItems.Count);
+        Debug.Log("item: " + allItems[2].name);
+    }
 
+    private void GetSpawnPoints()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            allSpawnPoints.Add(gameObject.transform.GetChild(i));
+        }
+        Debug.Log("allSpawns: " + allSpawnPoints.Count);
     }
 
     private void SpawnItem()
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < allSpawnPoints.Count; i++)
+        {
+            var randomItem = allItems[UnityEngine.Random.Range(0, allItems.Count)];
+
+            var modifyer = Instantiate(weaponModifyer, allSpawnPoints[i]);
+            modifyer.GetComponent<WeaponModifyerItem>().itemType = randomItem;
+        }
     }
 }
