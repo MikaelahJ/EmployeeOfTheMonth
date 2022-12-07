1e98e08080b4ee6be27361a59e8e5a2115037712
MikaelahJ <mikjen0129@skola.goteborg.se>
2022-12-07 17:01:20 +0100

    Glass crack almost fixed

--- Employee of the month/Assets/Scripts/ItemBreak.cs
+++ Employee of the month/Assets/Scripts/ItemBreak.cs
@@ -7,27 +7,24 @@ public class ItemBreak : MonoBehaviour
     [Header("Sprites in order first to last")]
     [SerializeField] private List<Sprite> spritesBeforeBreak = new List<Sprite>();
     [SerializeField] private Animator animator;
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     private int maxHealth;
     private int health;
 
     private void Start()
     {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        animator.enabled = false;
         spritesBeforeBreak.Reverse(); //känns enklast att kunna lägga in dom i rätt ordning i editorn så vi reversar den här ba
         maxHealth = spritesBeforeBreak.Count;
         health = maxHealth;
-
-        Debug.Log("maxhealth" + maxHealth);
     }
     public void TakeDamage()
     {
-
         health--;
-        Debug.Log("itemhealth" + health);
         if (health <= 0)
         {
+            animator.enabled = true;
             animator.SetTrigger("Break");
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
             return;
@@ -37,3 +34,28 @@ public class ItemBreak : MonoBehaviour
         Debug.Log("sprite " + spriteRenderer.sprite);
     }
 }
+
+//private int maxDamage;
+//private int damageTaken = 0;
+
+//private void Start()
+//{
+//    animator.enabled = false;
+//    maxDamage = spritesBeforeBreak.Count + 1;
+//}
+//public void TakeDamage()
+//{
+
+//    damageTaken++;
+//    if (damageTaken <= 0)
+//    {
+//        animator.enabled = true;
+//        animator.SetTrigger("Break");
+//        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+//        return;
+//    }
+
+//    spriteRenderer.sprite = spritesBeforeBreak[health];
+//    Debug.Log("sprite " + spriteRenderer.sprite);
+//}
+
