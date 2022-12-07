82df3d631ce6de25e7a21d40557851af46e9856b
MikaelahJ <mikjen0129@skola.goteborg.se>
2022-12-07 13:18:18 +0100

    Tie functions, shows multiple players if they have the same points and that point is highest

--- Employee of the month/Assets/GameOver.cs
+++ Employee of the month/Assets/GameOver.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System;
+
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI winnerText;
     [SerializeField] private Image winnerImage;
+    [SerializeField] private RectTransform winnerSpritePosition;
+
+    [SerializeField] private TextMeshProUGUI TieNameText;
+    [SerializeField] private RectTransform TieNames;
+    [SerializeField] private RectTransform TieSprites;
 
     public List<Sprite> winnerSprites = new List<Sprite>();
+
     private void Start()
     {
         CheckWinner();
@@ -18,10 +25,17 @@ public class GameOver : MonoBehaviour
 
     public void CheckWinner()
     {
-        int winner = GameManager.Instance.GetWinner();
-        int winnerSprite = GameManager.Instance.GetWinnerSprite(winner);
-
-        SetWinnerUI(winner, winnerSprite);
+        List<int> winner = GameManager.Instance.GetWinner();
+        Debug.Log("winnerCount " + winner.Count);
+        if (winner.Count == 1)
+        {
+            int winnerSprite = GameManager.Instance.GetWinnerSprite(winner[0]);
+            SetWinnerUI(winner[0], winnerSprite);
+        }
+        else
+        {
+            SetTieUI(winner);
+        }
     }
 
     public void SetWinnerUI(int playerIndex, int playerSprite)
@@ -32,16 +46,59 @@ public class GameOver : MonoBehaviour
         switch (playerSprite)
         {
             case 1:
-                winnerImage.sprite = winnerSprites[0];
+                var image = Instantiate(winnerImage);
+                image.sprite = winnerSprites[0];
                 break;
             case 2:
-                winnerImage.sprite = winnerSprites[1];
+                var image2 = Instantiate(winnerImage);
+                image2.sprite = winnerSprites[1];
                 break;
             case 3:
-                winnerImage.sprite = winnerSprites[2];
+                var image3 = Instantiate(winnerImage);
+                image3.sprite = winnerSprites[2];
                 break;
             case 4:
-                winnerImage.sprite = winnerSprites[3];
+                var image4 = Instantiate(winnerImage);
+                image4.sprite = winnerSprites[3];
+                break;
+        }
+    }
+    private void SetTieUI(List<int> winners)
+    {
+        winnerText.text = "TIE";
+
+        foreach (int winner in winners)
+        {
+            if (GameManager.Instance.players.ContainsKey("P" + winner))
+            {
+                var name = Instantiate(TieNameText, TieNames);
+                name.text += (winner + 1);
+
+                int playerSprite = GameManager.Instance.players["P" + winner];
+                SetSpriteImages(playerSprite);
+            }
+        }
+    }
+
+    private void SetSpriteImages(int playerSprite)
+    {
+        switch (playerSprite)
+        {
+            case 1:
+                var image = Instantiate(winnerImage, TieSprites);
+                image.sprite = winnerSprites[0];
+                break;
+            case 2:
+                var image2 = Instantiate(winnerImage, TieSprites);
+                image2.sprite = winnerSprites[1];
+                break;
+            case 3:
+                var image3 = Instantiate(winnerImage, TieSprites);
+                image3.sprite = winnerSprites[2];
+                break;
+            case 4:
+                var image4 = Instantiate(winnerImage, TieSprites);
+                image4.sprite = winnerSprites[3];
                 break;
         }
     }
