5a269d4d631718a7e4f3673d3ed1192648bc6674
MikaelahJ <mikjen0129@skola.goteborg.se>
2022-12-19 11:42:17 +0100

    Fixed microwave sprite not dissapearing on discard

--- Employee of the month/Assets/Scripts/Weapon Scripts/WeaponController.cs
+++ Employee of the month/Assets/Scripts/Weapon Scripts/WeaponController.cs
@@ -17,6 +17,8 @@ public class WeaponController : MonoBehaviour
     public UIItemHolder itemHolder;
 
     private AudioSource sound;
+
+    [SerializeField] private Laser laserScript;
     void Start()
     {
         items = new NewItemScriptableObject[itemSlots];
@@ -64,6 +66,8 @@ public class WeaponController : MonoBehaviour
 
     public void RemoveAllItems()
     {
+        laserScript.DiscardSuperSprite();
+
         for (int i = 0; i < items.Length; i++)
         {
             RemoveItem(i, false);
